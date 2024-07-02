6573a1ae9f5f45852d7e0cfc2d1d856e3f710473
Praise Nwanguma <131485897+Bezaleelstone@users.noreply.github.com>
2024-07-02 10:24:54 +0100

    Update provider.tf

--- terraform-aws/provider.tf
+++ terraform-aws/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region     = "us-east-2"
-  access_key = "AKIAZQ3DUQKIBFFE4MWA"
-  secret_key = "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"
+  access_key = ""
+  secret_key = "4"
 
 }
